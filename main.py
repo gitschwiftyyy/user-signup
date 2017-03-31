14d429858cc49d06a49121804ec9fb1bbceea1fd
schwifty <jcmcmanemin@gmail.com>
2017-03-30 19:36:13 -0500

    html header/footer and forms

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import webapp2
+import re
 
+content_header = """
+<!DOCTYPE html>
+<html>
+<head>
+  <title>User Signup</title>
+</head>
+<body>
+<t1>User Signup</t1>
+"""
+
+content_footer = """
+</body>
+</html>
+"""
+form = """
+<form action='/welcome' method='post'>
+<label>
+  Username <input type='text' name='username'/>
+</label><br>
+<label>
+  Password <input type='password' name='password'/>
+</label><br>
+<label>
+  Re-enter Password <input type='password' name='verify'/>
+</label><br>
+<label>
+  (Optional) Email <input type='text' name='email'/>
+</label><br>
+<input type='submit' value='Submit'/>
+</form>
+"""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+      content = content_header + form + content_footer
+      self.response.write(content)
 
 
 app = webapp2.WSGIApplication([
