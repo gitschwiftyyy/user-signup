08bbaf0c88426f454c20230ad853f78568c0dd55
schwifty <jcmcmanemin@gmail.com>
2017-03-31 16:05:54 -0500

    consolidatated (sp?) signup and main pages

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ content_footer = """
 """
 
 form = """
-<form action='/signup' method='post'>
+<form action='/' method='post'>
 <label>
   Username: <input type='text' name='username' value='%(username)s'/>{0}
 </label><br>
@@ -42,6 +42,58 @@ class MainPage(webapp2.RequestHandler):
     def get(self, username_error = "", password_error = "", verify_error = "", email_error = "", username = "", password = "", verify = "", email = ""):
       
       content = content_header + form.format(username_error, password_error, verify_error, email_error) % {"username":username, "password":password, "verify":verify, "email":email} + content_footer
+      self.response.write(content)
+    
+    def post(self,username_error = "",password_error = "",verify_error = "",email_error = "", username = "", password = "", verify = "", email = ""):
+      username = self.request.get('username')
+      username = cgi.escape(username,quote=True)
+      password = self.request.get('password')
+      password = cgi.escape(password,quote=True)
+      verify = self.request.get('verify')
+      verify = cgi.escape(verify,quote=True)
+      email = self.request.get('email')
+      email = cgi.escape(email,quote=True)
+      
+      username_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+      password_re = re.compile(r".{3,20}$")
+      email_re = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+      
+      def valid_username(username):
+        return username_re.match(username)
+      def valid_password(password):
+        return password_re.match(password)
+      def valid_email(email):
+        if email == "":
+          return True
+        else:
+          return email_re.match(email)
+        
+        
+      if not valid_username(username):
+        username_error = "<strong>   *Username must be 3-20 characters and may contain only numbers, letters, '-', and '_'*</strong>"
+        if username == "":
+          username_error = "<strong>   *Please enter a username*</strong>"
+      
+      if not valid_password(password):
+        password_error = "<strong>   *Password must be 3-20 characters in length*</strong>"
+        if password == "":
+          password_error = "<strong>   *Please enter a password*</strong>"
+      
+      if verify != password:
+        verify_error = "<strong>   *Passwords did not match*</strong>"
+        if verify == "":
+          verify_error = "<strong>   *Please re-enter your password*</strong>"
+      
+      if verify == "":
+        verify_error = "<strong>   *Please re-enter your password*</strong>"
+      
+      if not valid_email(email):
+        email_error = "<strong>   *Please enter a valid email*</strong>"
+      
+      if valid_username(username) and valid_password(password) and password == verify and valid_email(email):
+        content = content_header + thank_you + content_footer
+      else:
+        content = content_header + form.format(username_error, password_error, verify_error, email_error) % {"username":username, "password":password, "verify":verify, "email":email} + content_footer
       self.response.write(content)
 
 class SignupPage(webapp2.RequestHandler):
